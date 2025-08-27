c9d16805b94ede04fd84764e7101f2bc75f95ef5
Mustafa Mervan Akbulut <musakbul@k1m31s07.42istanbul.com.tr>
2025-08-27 16:26:42 +0300

    work with stdin

--- main.c
+++ main.c
@@ -1,25 +1,17 @@
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <stdio.h>
 
 int main(void)
 {
-    int fd = open("test.txt", O_RDONLY);
-	char *result = get_next_line(fd);
-	printf("%s\n", result);
-	free(result);
+    int fd = 0;
+	char *result;
 	
-	result = get_next_line(fd);
-	printf("%s\n", result);
-	free(result);
+    while ((result = get_next_line(fd)) != NULL)
+    {
+        printf("%s", result);
+        free(result);
+    }
 
-	result = get_next_line(fd);
-	printf("%s\n", result);
-	free(result);
-
-	result = get_next_line(fd);
-	printf("%s\n", result);
-	free(result);
-
-	close(fd);
     return (0);
 }
